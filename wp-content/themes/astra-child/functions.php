531cbecedcd478eef81de56e9a14dbb3a92ae27e
Richa <richaparekh.work@gmail.com>
2026-08-10 19:03:44 +0530

    feat: create child theme

---
+++ wp-content/themes/astra-child/functions.php
@@ -0,0 +1,7 @@
+<?php
+// Your code to enqueue parent theme styles
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+?>
